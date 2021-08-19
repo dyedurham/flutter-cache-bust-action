a458dd7976c51da8036df5d9923f2776498c8252
Laura Berger-Uichanco <laura.berger@globalx.com.au>
2021-08-19 15:37:00 +1000

    fix: rebuild source

--- dist/index.js
+++ dist/index.js
@@ -12040,8 +12040,9 @@ try {
     const jsPath = `${buildPath}/main.dart.js`;
     const flutterServiceWorkerPath = `${buildPath}/flutter_service_worker.js`;
 
-    fs.rename(jsPath, `${buildPath}/${filename}`);
-    console.log(`Renaming main.dart.js to ${filename}`);
+    fs.rename(jsPath, `${buildPath}/${filename}`, () => {
+        console.log(`Renamed main.dart.js to ${filename}`);
+    });
 
     const options = {
         files: [ htmlPath, flutterServiceWorkerPath ],
@@ -12054,7 +12055,6 @@ try {
 } catch (error) {
     core.setFailed(error.message);
 }
-
 })();
 
 module.exports = __webpack_exports__;
